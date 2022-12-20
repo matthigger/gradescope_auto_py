a9987c9a907b3f9242635e97d706ee2d443009f6
matt higger <matt.higger@gmail.com>
2022-12-20 15:18:59 -0500

    revise - setup.sh - we now install from requirements.txt (it was in source directory).  also no more venv ... mroe challenging to test and not meaningful on gradescope anyways (its already in a container)

--- gradescope_auto_py/gradescope/setup.sh
+++ gradescope_auto_py/gradescope/setup.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# install python pips
-apt-get install python3-pip
-pip3 install virtualenv
-
 # setup virtual environment and install necessary packages
-virtualenv venv
-source venv/bin/active
 pip3 install gradescope_auto_py
-pip3 install -r requirements.txt
+pip3 install -r source/requirements.txt
