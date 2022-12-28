1fd19fdffe7fdeea558bdbddf01f7a42fe38af92
matt higger <matt.higger@gmail.com>
2022-12-28 07:24:06 -0500

    revise - GraderConfig - now has "file_run", the name of the file to be run to perform autograding.  as a result, we change storage format to json (from txt) see #11

--- gradescope_auto_py/grader_config.py
+++ gradescope_auto_py/grader_config.py
@@ -1,52 +1,73 @@
+import json
+import pathlib
 from collections import Counter
 
 from gradescope_auto_py.assert_for_pts import AssertForPoints
 
 
-class GraderConfig(list):
-    """ a 'configuration' for an assignment, a list of AssertForPoints
+class GraderConfig:
+    """ a 'configuration' for an assignment
+
+    Attributes:
+        file_run (str): file to run for autograding (often student submitted
+            version of assignment)
+        afp_list (list): a list of AssertForPoints
 
     GraderConfig is intended to be created from some canonical copy of the
     HW, rather than a student submission.  this ensures fidelity of all
     asserts (and point values) to the canonical copy.
     """
 
-    def to_txt(self, file):
+    def __init__(self, file_run, afp_list):
+        self.file_run = pathlib.Path(file_run).name
+        self.afp_list = afp_list
+
+    def to_json(self, file):
         """ writes config to txt file (string of each assert on each line)
 
         Args:
             file (str): file to write configuration to
         """
-        with open(str(file), 'w') as f:
-            print('\n'.join([afp.s for afp in self]), file=f)
+        json_dict = {'file_run': self.file_run,
+                     'afp_list': [afp.s for afp in self.afp_list]}
+        with open(file, 'w') as f:
+            json.dump(json_dict, f, sort_keys=True, indent=4)
 
     @classmethod
-    def from_txt(cls, file):
+    def from_json(cls, file):
         """ reads GraderConfig from txt file
 
         Args:
             file (str): file to write configuration to
         """
-        with open(file, 'r') as f:
-            afp_s_list = f.read().strip().split('\n')
+        with open(file) as f:
+            json_dict = json.load(f)
 
-        return GraderConfig([AssertForPoints(s=s) for s in afp_s_list])
+        afp_list = [AssertForPoints(s=s) for s in json_dict['afp_list']]
+        return GraderConfig(file_run=json_dict['file_run'],
+                            afp_list=afp_list)
 
     @classmethod
-    def from_py(cls, file):
+    def from_py(cls, file, file_run=None):
         """ builds configuration file for assignment from a given rubric
 
         Args:
             file (str): an input .py file (student or rubric copy)
+            file_run (str): name of file to run to perform autograding.  if not
+                passed, file name is used
 
         Returns:
             grader_config (GraderConfig):
         """
+        if file_run is None:
+            # default file_run name (same as input py file)
+            file_run = pathlib.Path(file).name
+
         # read in only assert for points (strings) from file
-        grader_config = GraderConfig(AssertForPoints.iter_assert_for_pts(file))
+        afp_list = list(AssertForPoints.iter_assert_for_pts(file))
 
         # ensure each is unique
-        for afp, count in Counter(grader_config).items():
+        for afp, count in Counter(afp_list).items():
             assert count < 2, f'non-unique assert found: {afp.s}'
 
-        return grader_config
+        return GraderConfig(afp_list=afp_list, file_run=file_run)
--- test/test_grader_config.py
+++ test/test_grader_config.py
@@ -1,11 +1,20 @@
+import tempfile
+
 from gradescope_auto_py.grader_config import GraderConfig
 
 
 def test_write_config():
-    afp_list = GraderConfig.from_py(file='ex_assign.py')
-    afp_s_list = [afp.s for afp in afp_list]
+    # test __init__ & from_py()
+    grader_config = GraderConfig.from_py(file='ex_assign.py')
+    assert grader_config.file_run == 'ex_assign.py'
+    assert len(grader_config.afp_list) == 3
 
-    with open('ex_config.txt', 'r') as f:
-        afp_s_list_expected = f.read().split('\n')
+    # test from_json()
+    grader_config_exp = GraderConfig.from_json('ex_config.json')
+    assert grader_config.__dict__ == grader_config_exp.__dict__
 
-    assert afp_s_list == afp_s_list_expected
+    # test to_json()
+    file = tempfile.NamedTemporaryFile(suffix='.json').name
+    grader_config.to_json(file=file)
+    grader_config2 = GraderConfig.from_json(file=file)
+    assert grader_config2.__dict__ == grader_config.__dict__
